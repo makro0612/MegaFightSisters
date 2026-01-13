d346eca7486e6c1ddf6df83b4b90b4315e073c8c
maje0401 <146211476+maje0401@users.noreply.github.com>
2026-01-13 12:56:56 +0100

    Fiksa main

--- main.py
+++ main.py
@@ -5,12 +5,10 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-VINDU_BREDDE = 500
-VINDU_HOYDE = 500
 FPS = 60
 
 pg.init()
-vindu = pg.display.set_mode([VINDU_BREDDE, VINDU_HOYDE])
+vindu = pg.display.set_mode((0,0),pg.FULLSCREEN)
 clock = pg.time.Clock()
 
 
@@ -23,7 +21,7 @@ def main():
             elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
                 running = False
 
-        vindu.fill(WHITE)
+        vindu.fill(BLUE)
         
 
         pg.display.flip()
