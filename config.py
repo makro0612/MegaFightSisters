32680803f96a672b9ea416843cefffa2cef2940c
maje0401 <146211476+maje0401@users.noreply.github.com>
2026-01-15 10:29:42 +0100

    direction

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
-colors = {
+COLORS = {
     "WHITE": (255, 255, 255),
     "RED": (255, 0, 0),
     "GREEN": (0, 255, 0),
-    "BLUE": (0, 0, 255)}
+    "BLUE": (0, 0, 255),
+    "BLACK": (0,0,0)}
 WINDOWHEIGHT = 540
 WINDOWWIDTH = 960
 FPS = 60
